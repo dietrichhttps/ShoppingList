d73b1c4e1f07eebd8fdbf16e8b06873a61dcd27b
aliceglassPC <abayhanov1998@gmail.com>
2025-01-16 01:33:18 +0300

    Changed getShopList return type to LiveData<List<ShopItem>>

--- app/src/main/java/com/example/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglist/presentation/MainActivity.kt
@@ -10,15 +10,20 @@ import com.example.shoppinglist.domain.ShopItem
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: MainViewModel
+
+    private var count = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
         viewModel.shopList.observe(this) {
             Log.d("MainActivity", it.toString())
+            if (count == 0) {
+                count++
+                val item = it[0]
+                viewModel.changeEnableState(item)
+            }
         }
-        viewModel.deleteShopItem(ShopItem("Name 0", 0, true, 0))
-        viewModel.changeEnableState(ShopItem("Name 1", 1, true, 1))
     }
 }
 
